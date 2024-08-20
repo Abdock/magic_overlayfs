b3d2802c3211f55d2a9949ee8e9392aa605674d3
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2024-08-20 17:07:10 +0600

    idk

--- magisk-module/mode.sh
+++ magisk-module/mode.sh
@@ -3,8 +3,9 @@
 # 1 - read-write default
 # 2 - read-only locked (cannot remount as read-write)
 # You can set to 2 after modify system partititons to avoid detection
-export OVERLAY_MODE=0
+export OVERLAY_MODE=1
 
 # Set to true to mount overlayfs on subdirectories of /system /vendor /system_ext /product,...
 # instead of on /system /vendor /system_ext /product,...
-export OVERLAY_LEGACY_MOUNT=true
+export OVERLAY_LEGACY_MOUNT=false
+export DO_UNMOUNT_KSU=true
